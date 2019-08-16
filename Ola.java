eb596f6a1161dce8bd062c01243367f07f36527d
jose.sbrandao <jose.sbrandao@gmail.com>
2019-08-16 08:26:29 -0300

    Revisão das aulas de Java

---
+++ Ola.java
@@ -0,0 +1,15 @@
+
+public class Ola{
+    public static void main(String[] args){
+        System.out.println("Bem Vindo ao Mundo Java!");
+int a = 5;
+
+System.out.println(a+5);
+	try{
+System.out.println(a/0);
+} catch(Exception e){
+	System.out.println("Something went wrong. " + e);
+}
+}
+}
+
